22d34d81c8261210ef3a219e482c5ee2f486b4ca
cj <chunaj300@gmail.com>
2022-05-25 15:53:59 +0300

    new branch

--- app/src/main/java/com/example/englishdictionary/favorite/TextViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.englishdictionary.favorite
-
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-
-class TextViewHolder( val textView:TextView) :RecyclerView.ViewHolder(textView)
--- app/src/main/java/com/example/englishdictionary/network/dataClasses/UserError.kt
+++
@@ -1,7 +0,0 @@
-package com.example.englishdictionary.network.dataClasses
-
-data class UserError(
-    val message: String,
-    val resolution: String,
-    val title: String
-)
