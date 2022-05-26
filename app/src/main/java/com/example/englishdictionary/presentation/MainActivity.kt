6a10cd26f4a6d798f723efad815022fa38c33a00
cj <chunaj300@gmail.com>
2022-05-27 00:19:31 +0300

    ui start

--- app/src/main/java/com/example/englishdictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/example/englishdictionary/presentation/MainActivity.kt
@@ -2,9 +2,20 @@ package com.example.englishdictionary.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.englishdictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var _binding:ActivityMainBinding? = null
+    val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(this.layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
