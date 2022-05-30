fe02e8e93a5df007237f4ee7af30a4dce445e1a6
cj <chunaj300@gmail.com>
2022-05-30 11:08:33 +0300

    update

--- app/src/main/java/com/example/englishdictionary/presentation/main/MainActivityViewModel.kt
+++ app/src/main/java/com/example/englishdictionary/presentation/main/MainActivityViewModel.kt
@@ -3,9 +3,18 @@ package com.example.englishdictionary.presentation.main
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.englishdictionary.core.util.Resource
 import com.example.englishdictionary.domain.use_case.GetWordInfo
 import com.example.englishdictionary.presentation.WordInfoState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,5 +29,49 @@ constructor(
     private val _state = mutableStateOf(WordInfoState())
     val state: State<WordInfoState> = _state
 
-    
+    private  val _eventFlow  = MutableSharedFlow<UIEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private var searchJob:Job? = null
+
+    fun onSearch(query:String) {
+        _searchQuery.value = query
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            getWordInfo.invoke(query)
+                .onEach {result ->
+                    when(result) {
+                        is Resource.Success ->  {
+                            _state.value = _state.value.copy(
+                                wordInfoState = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                        }
+                        is Resource.Loading -> {
+                            _state.value = _state.value.copy(
+                                wordInfoState = result.data ?: emptyList(),
+                                isLoading = true
+                            )
+                        }
+                        is Resource.Error -> {
+                            _state.value = _state.value.copy(
+                                wordInfoState = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                            _eventFlow.emit(
+                                UIEvent.ShowSnackBar(
+                                    result.message ?: "Unknown error"
+                                )
+                            )
+                        }
+                    }
+                }.launchIn(this)
+        }
+    }
+
+
+  sealed class UIEvent {
+      data class ShowSnackBar(val message:String):UIEvent()
+  }
 }
